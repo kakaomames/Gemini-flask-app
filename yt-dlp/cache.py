dd78eaafbe5e7f05a5d4d3f607b21421c3624c3c
kakaomame <nattyannkunndesu@icloud.com>
2025-09-16 10:55:17 +0900

    Add files via upload

--- yt-dlp/cache.py
+++ yt-dlp/cache.py
@@ -1,91 +1,97 @@
-import contextlib
-import json
-import os
-import re
-import shutil
-import traceback
-import urllib.parse
+"""PUBLIC API"""
 
-from .utils import expand_path, traverse_obj, version_tuple, write_json_file
-from .version import __version__
+from __future__ import annotations
+
+import abc
+import dataclasses
+import enum
+import typing
+
+from yt_dlp.extractor.youtube.pot._provider import (
+    IEContentProvider,
+    IEContentProviderError,
+    register_preference_generic,
+    register_provider_generic,
+)
+from yt_dlp.extractor.youtube.pot._registry import (
+    _pot_cache_provider_preferences,
+    _pot_cache_providers,
+    _pot_pcs_providers,
+)
+from yt_dlp.extractor.youtube.pot.provider import PoTokenRequest
 
 
-class Cache:
-    def __init__(self, ydl):
-        self._ydl = ydl
+class PoTokenCacheProviderError(IEContentProviderError):
+    """An error occurred while fetching a PO Token"""
 
-    def _get_root_dir(self):
-        res = self._ydl.params.get('cachedir')
-        if res is None:
-            cache_root = os.getenv('XDG_CACHE_HOME', '~/.cache')
-            res = os.path.join(cache_root, 'yt-dlp')
-        return expand_path(res)
 
-    def _get_cache_fn(self, section, key, dtype):
-        assert re.match(r'^[\w.-]+$', section), f'invalid section {section!r}'
-        key = urllib.parse.quote(key, safe='').replace('%', ',')  # encode non-ascii characters
-        return os.path.join(self._get_root_dir(), section, f'{key}.{dtype}')
+class PoTokenCacheProvider(IEContentProvider, abc.ABC, suffix='PCP'):
+    @abc.abstractmethod
+    def get(self, key: str) -> str | None:
+        pass
 
-    @property
-    def enabled(self):
-        return self._ydl.params.get('cachedir') is not False
+    @abc.abstractmethod
+    def store(self, key: str, value: str, expires_at: int):
+        pass
 
-    def store(self, section, key, data, dtype='json'):
-        assert dtype in ('json',)
+    @abc.abstractmethod
+    def delete(self, key: str):
+        pass
 
-        if not self.enabled:
-            return
 
-        fn = self._get_cache_fn(section, key, dtype)
-        try:
-            os.makedirs(os.path.dirname(fn), exist_ok=True)
-            self._ydl.write_debug(f'Saving {section}.{key} to cache')
-            write_json_file({'yt-dlp_version': __version__, 'data': data}, fn)
-        except Exception:
-            tb = traceback.format_exc()
-            self._ydl.report_warning(f'Writing cache to {fn!r} failed: {tb}')
+class CacheProviderWritePolicy(enum.Enum):
+    WRITE_ALL = enum.auto()    # Write to all cache providers
+    WRITE_FIRST = enum.auto()  # Write to only the first cache provider
 
-    def _validate(self, data, min_ver):
-        version = traverse_obj(data, 'yt-dlp_version')
-        if not version:  # Backward compatibility
-            data, version = {'data': data}, '2022.08.19'
-        if not min_ver or version_tuple(version) >= version_tuple(min_ver):
-            return data['data']
-        self._ydl.write_debug(f'Discarding old cache from version {version} (needs {min_ver})')
 
-    def load(self, section, key, dtype='json', default=None, *, min_ver=None):
-        assert dtype in ('json',)
+@dataclasses.dataclass
+class PoTokenCacheSpec:
+    key_bindings: dict[str, str | None]
+    default_ttl: int
+    write_policy: CacheProviderWritePolicy = CacheProviderWritePolicy.WRITE_ALL
 
-        if not self.enabled:
-            return default
+    # Internal
+    _provider: PoTokenCacheSpecProvider | None = None
 
-        cache_fn = self._get_cache_fn(section, key, dtype)
-        with contextlib.suppress(OSError):
-            try:
-                with open(cache_fn, encoding='utf-8') as cachef:
-                    self._ydl.write_debug(f'Loading {section}.{key} from cache')
-                    return self._validate(json.load(cachef), min_ver)
-            except (ValueError, KeyError):
-                try:
-                    file_size = os.path.getsize(cache_fn)
-                except OSError as oe:
-                    file_size = str(oe)
-                self._ydl.report_warning(f'Cache retrieval from {cache_fn} failed ({file_size})')
 
-        return default
+class PoTokenCacheSpecProvider(IEContentProvider, abc.ABC, suffix='PCSP'):
 
-    def remove(self):
-        if not self.enabled:
-            self._ydl.to_screen('Cache is disabled (Did you combine --no-cache-dir and --rm-cache-dir?)')
-            return
+    def is_available(self) -> bool:
+        return True
 
-        cachedir = self._get_root_dir()
-        if not any((term in cachedir) for term in ('cache', 'tmp')):
-            raise Exception(f'Not removing directory {cachedir} - this does not look like a cache dir')
+    @abc.abstractmethod
+    def generate_cache_spec(self, request: PoTokenRequest) -> PoTokenCacheSpec | None:
+        """Generate a cache spec for the given request"""
+        pass
 
-        self._ydl.to_screen(
-            f'Removing cache dir {cachedir} .', skip_eol=True)
-        if os.path.exists(cachedir):
-            self._ydl.to_screen('.', skip_eol=True)
-            shutil.rmtree(cachedir)
-        self._ydl.to_screen('.')
+
+def register_provider(provider: type[PoTokenCacheProvider]):
+    """Register a PoTokenCacheProvider class"""
+    return register_provider_generic(
+        provider=provider,
+        base_class=PoTokenCacheProvider,
+        registry=_pot_cache_providers.value,
+    )
+
+
+def register_spec(provider: type[PoTokenCacheSpecProvider]):
+    """Register a PoTokenCacheSpecProvider class"""
+    return register_provider_generic(
+        provider=provider,
+        base_class=PoTokenCacheSpecProvider,
+        registry=_pot_pcs_providers.value,
+    )
+
+
+def register_preference(
+        *providers: type[PoTokenCacheProvider]) -> typing.Callable[[CacheProviderPreference], CacheProviderPreference]:
+    """Register a preference for a PoTokenCacheProvider"""
+    return register_preference_generic(
+        PoTokenCacheProvider,
+        _pot_cache_provider_preferences.value,
+        *providers,
+    )
+
+
+if typing.TYPE_CHECKING:
+    CacheProviderPreference = typing.Callable[[PoTokenCacheProvider, PoTokenRequest], int]
